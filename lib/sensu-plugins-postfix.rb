b1cee0f981f2b5c4294cb4ef788849548a96691b
Tim Smith <tim@cozy.co>
2015-05-29 23:39:08 -0700

    Update version library

--- lib/sensu-plugins-postfix.rb
+++ lib/sensu-plugins-postfix.rb
@@ -1,7 +1,15 @@
+
+require 'sensu-plugins-postfix/version'
+
+# Load the defaults
+
 #
-# Set gem version
+# Default class
 #
 module SensuPluginsPostfix
-  # Gem version
-  VERSION = '0.0.1.alpha.1'
+  class << self
+  end
+
+  class << self
+  end
 end
