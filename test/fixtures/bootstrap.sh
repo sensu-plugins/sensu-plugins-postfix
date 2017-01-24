3f05fa60e2353d34b3eccb98e41c9e316068f357
Jonathan Hartman <j@p4nt5.com>
2017-01-24 10:16:21 -0800

    Get the CI tests passing again
    Mostly the same thing as
    https://github.com/sensu-plugins/sensu-plugins-http/pull/60. This gets
    Sensu's embedded Ruby and Travis RVM to quit stomping on each other's
    environment variables and causing the integration tests to fail.

--- test/fixtures/bootstrap.sh
+++ test/fixtures/bootstrap.sh
@@ -1,23 +1,28 @@
-#!/bin/sh
+#!/bin/bash
 #
 # Set up a super simple Postfix server and block its outbound port 25 so we can
 # use it for testing.
 #
 
+set -e
+
+source /etc/profile
 DATA_DIR=/tmp/kitchen/data
-SENSU_DIR=/opt/sensu
-GEM=$SENSU_DIR/embedded/bin/gem
-RUBY=$SENSU_DIR/embedded/bin/ruby
+RUBY_HOME=${MY_RUBY_HOME:-/opt/sensu/embedded}
 
-if [ ! -d $SENSU_DIR ]; then
-  wget -q http://repositories.sensuapp.org/apt/pubkey.gpg -O- | sudo apt-key add -
-  echo "deb http://repositories.sensuapp.org/apt sensu main" | sudo tee /etc/apt/sources.list.d/sensu.list
-
-  sudo apt-get update
-  sudo DEBIAN_FRONTEND=noninteractive apt-get install -y git vim postfix mailutils sensu
-  sudo iptables -I OUTPUT -m tcp -p tcp --dport 25 -j DROP
+if [ "$RUBY_HOME" = "/opt/sensu/embedded" ] && [ ! -d $RUBY_HOME ]; then
+  wget -q http://repositories.sensuapp.org/apt/pubkey.gpg -O- | apt-key add -
+  echo "deb http://repositories.sensuapp.org/apt sensu main" > /etc/apt/sources.list.d/sensu.list
+  apt-get update
+  apt-get install -y sensu
+else
+  apt-get update
 fi
 
+DEBIAN_FRONTEND=noninteractive apt-get install -y build-essential postfix mailutils
+iptables -I OUTPUT -m tcp -p tcp --dport 25 -j DROP
+service postfix restart
+
 cd $DATA_DIR
-SIGN_GEM=false $GEM build sensu-plugins-postfix.gemspec
-sudo sensu-install -p sensu-plugins-postfix-*.gem
+SIGN_GEM=false $RUBY_HOME/bin/gem build sensu-plugins-postfix.gemspec
+$RUBY_HOME/bin/gem install sensu-plugins-postfix-*.gem
